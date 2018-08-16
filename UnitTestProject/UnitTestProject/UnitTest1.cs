60e0f8854dfe38533c4f6d20bfdc1e7ff68c9638
parthaf22@gmail.xom <partha.s@excelindia.com>
2018-08-16 14:09:59 +0530

    Configuration added

---
+++ UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver wd;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            wd = new ChromeDriver();
+
+            wd.Manage().Window.Maximize();
+
+            wd.Url = "";
+
+            string currenttitle = wd.Title;
+
+            wd.FindElement(By.Name("q")).SendKeys("");
+
+            wd.Quit();
+
+        }
+    }
+}
